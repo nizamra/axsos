9fafe9672d702357d7c98fdadd5a0ae1f59759a9
Amro Abuhashish <aasahashish@gmail.com>
2021-04-06 13:04:18 +0300

    added 3 ninja html

---
+++ Js/amro.js
@@ -0,0 +1,13 @@
+prompt.start();
+var temp = prompt("Please enter the temperature: ");
+if(temp>=30){
+console.log("it is Hot")
+var tem=15
+console.log(tem)
+}
+else if(temp<30 && temp>20){
+console.log("it is Moderate")
+}
+else if(temp<=20){
+console.log("it is cold")
+}
